7140e98b14734ec554899cc6d2dc9201ad8ae475
aymane smini <aymane.smini@gmail.com>
2024-07-29 17:59:37 +0100

    Writing a class

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -9,4 +9,32 @@ interface Teacher  {
 
 interface Directors extends Teacher {
   numberOfReports: number;
+};
+
+interface StudentClassInterface {
+  _firstName: string;
+  _lastName: string;
+};
+
+interface StudentClassConstructorInterface {
+  new (firstName: string, lastName: string): StudentClassInterface;
+}
+class StudentClass implements StudentClassInterface {
+  _firstName : string;
+  _lastName : string;
+
+  constructor(firstName: string, lastName: string) {
+    this._firstName = firstName;
+    this._lastName = lastName;
+  }
+
+  workOnHomework() {
+    return 'Currently working';
+  }
+
+  displayName() {
+    return this._firstName;
+  }
+
+
 }
